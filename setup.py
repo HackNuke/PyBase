0e9bf9c6194d3d47346c3b59d21eeef0e28c3c1c
NTBBloodbath <CaptainSpark365@gmail.com>
2020-12-05 23:02:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#    NTB Bloodbath
+#    PyBase Team
 #
 
 from setuptools import setup, find_packages
@@ -11,9 +11,9 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pybase_db',
-    version='0.4.1',
+    version='0.5.0',
     description=
-    'PyBase is a database manager for YAML, JSON, Bytes and SQLite3. Very poweful, simple and effective.',
+    'PyBase is a database manager for multiple filetypes including SQLite3. Very poweful, simple and effective.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='http://ntbbloodbath.github.io/PyBase/',
@@ -21,9 +21,13 @@ setup(
     author_email='bloodbathalchemist@protonmail.com',
     license='MIT',
     packages=find_packages(),
-    install_requires=['pyyaml>=5.3.1', 'rich>=6.2.0'],
+    install_requires=['pyyaml>=5.3.1', 'rich>=9.2.0'],
     classifiers=[
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
